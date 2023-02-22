799eb5602cab1df01f413eabbe2123dc90897be0
Fernando Toledo <fernandohtr@gmail.com>
2023-02-22 09:52:44 -0300

    Refactor order of features and tie status

--- src/index.js
+++ src/index.js
@@ -13,6 +13,26 @@ function Square(props) {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -23,7 +43,7 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
-    const squares = this.state.squares.slice();
+    const squares = this.state.squares;
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -48,7 +68,9 @@ class Board extends React.Component {
     let status;
     if (winner) {
       status = `Winner: ${winner}`;
-    } else {
+    } else if (!this.state.squares.includes(null)) {
+      status = 'Draw!';
+    }else {
       status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
     }
 
@@ -73,26 +95,6 @@ class Board extends React.Component {
       </>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 class Game extends React.Component {
